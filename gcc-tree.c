30abf7a796496c4383d2996a896a0718a8a14b79
mike632t <1799044+mike632t@users.noreply.github.com>
2025-05-26 19:33:35 +0100

    Creates binary tree using strings

--- gcc-tree.c
+++ gcc-tree.c
@@ -20,39 +20,42 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
  * 20 May 25  0.1  001 - Initial version - MT
+ * 25 May 25  0.2  002 - Creates binary tree using strings - MT
  *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct leaf
 {
-   int data;
+   char *data;
    struct leaf *left;
    struct leaf *right;
 };
 
 typedef struct leaf leaf;
 
-leaf *h_new(int i_data) /* Create a new leaf */
+leaf *h_new(char *s_data) /* Create a new leaf */
 {
    leaf *h_leaf;
    h_leaf = (leaf*)malloc(sizeof(*h_leaf));
-   h_leaf->data = i_data;
+   h_leaf->data = s_data; /* Saves pointer to string in leaf */
    h_leaf->left = NULL;
    h_leaf->right = NULL;
    return h_leaf;
 }
-leaf *h_add(leaf *h_root, int i_value) /*  Adds a leaf to the tree */
+
+leaf *h_add(leaf *h_root, char *s_data) /*  Adds a leaf to the tree */
 {
    if (h_root != NULL)
-      if (i_value < h_root->data) 
-         h_root->left = h_add (h_root->left, i_value);
+      if (strcmp(h_root->data, s_data) > 0) 
+         h_root->left = h_add (h_root->left, s_data);
       else
-         h_root->right = h_add (h_root->right, i_value);
+         h_root->right = h_add (h_root->right, s_data);
    else
-      h_root = h_new(i_value); /* Create leaf */
+      h_root = h_new(s_data); /* Create leaf */
    return h_root;
 }
 
@@ -61,19 +64,22 @@ void v_inorder(leaf *h_root)
    if (h_root != NULL)
    {
       v_inorder(h_root->left); /* Print left sub-tree */
-      printf("%d ", h_root->data);
+      printf("%s ", h_root->data);
       v_inorder(h_root->right); /* Print right sub-tree */
    }
 }
 
 int main()
 {
-   int i_array[] = { 5, 1, 3, 8, 4, 9, 6, 0, 2, 7 };
-   
+   char *s_array[] = { "William", "Michael", "Lisa", "John", "Mary",
+                        "David", "James", "Karen", "Robert", "Linda",
+                        "Mark",  "Patricia", "Deborah", "Richard",
+                        "Elizabeth" };   
+
    leaf *h_root = NULL;
-   int i_size = sizeof(i_array) / sizeof(i_array[0]);
+   int i_size = sizeof(s_array) / sizeof(s_array[0]);
    for (int i_count = 0; i_count < i_size; ++i_count)
-      h_root = h_add(h_root, i_array[i_count]); /* Add values to tree */
+      h_root = h_add(h_root, s_array[i_count]); /* Add values to tree */
    v_inorder(h_root); /* Display tree */
    printf("\n");
 
